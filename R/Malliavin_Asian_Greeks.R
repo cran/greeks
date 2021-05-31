f78a1a34953c4281c68a1d64bf0a799078907a77
Anselm Hudde <anselmhudde@gmx.de>
2021-05-31 18:40:02 +0000

    version 0.2.0

--- R/Malliavin_Asian_Greeks.R
+++ R/Malliavin_Asian_Greeks.R
@@ -1,8 +1,10 @@
-#' This function calculates the fair value of an Asian call- or put-option by
-#' with the Malliavin Monte Carlo Method in the Black Scholes model.
+#' This function calculates the fair value of an European option by with the
+#' Malliavin Monte Carlo Method in the Black Scholes model.
+#'
 #' @export
 #'
-#' @importFrom "stats" "rnorm"
+#' @import "stats"
+#' @importFrom "matrixStats" "rowCumsums"
 #'
 #' @param initial_price - initial price of the underlying asset.
 #' @param exercise_price - strike price of the option.
@@ -12,11 +14,17 @@
 #' @param dividend_yield - dividend yield.
 #' @param payoff - the payoff function, either a string in ("put", "call"), or a
 #' function.
-#' @param greek - the greek to be calculated.
-#' @param model - the model to be chosen.
+#' @param greek - the Greek to be calculated.
+#' @param model - the model to be chosen in ("black_scholes", "jump_diffusion")
+#' @param lambda - the lambda of the Poisson process in the jump-diffusion model
+#' @param alpha - the alpha in the jump-diffusion model influences the jump size
+#' @param jump_distribution - the distribution of the jumps, choose a function
+#' which generates random numbers with the desired distribution
 #' @param steps - the number of integration steps.
 #' @param paths - the number of simulated paths.
 #' @param seed - the seed of the random number generator
+#' @param antithetic - if TRUE, antithetic random numbers will be chosen to
+#' decrease variance
 #'
 #' @return Named vector containing the values of the Greeks specified in the
 #' parameter \code{greek}.
@@ -33,80 +41,112 @@ Malliavin_Asian_Greeks <- function(initial_price = 100,
                                    volatility = 0.3,
                                    dividend_yield = 0,
                                    payoff = "call",
-                                   greek = c("fair_value", "delta", "rho",
-                                             "theta"),
-                                   model = "Black Scholes",
-                                   steps = time_to_maturity*252,
+                                   greek = c("fair_value", "delta", "rho", "vega",
+                                             "theta", "gamma"),
+                                   model = "black_scholes",
+                                   lambda = 0.2,
+                                   alpha = 0.3,
+                                   jump_distribution = function(n) stats::rt(n, df = 3),
+                                   steps = round(time_to_maturity*252),
                                    paths = 10000,
-                                   seed = 1) {
+                                   seed = 1,
+                                   antithetic = FALSE) {
 
   dt <- 1/steps
-
-  ## the seed is just set locally
-  old <- .Random.seed
-  on.exit( { .Random.seed <<- old } )
-
-  if(!is.na(seed)) {
-    set.seed(seed)
-  }
 
   result <- vector(mode = "numeric", length = length(greek))
 
   names(result) <- greek
 
-  ## the increments of the Brownian motion ###
-
-  W <- rnorm(n = paths*steps, sd = sqrt(dt)) %>%
-    matrix(nrow=paths) %>%
-    apply(MARGIN=1, FUN=cumsum) %>%
-    t()
-
-  W <- cbind(numeric(paths), W)
-
-  W_T <- W[, steps + 1]
-
-  ### the payoff function ###
+  ## the payoff function ##
 
   if(class(payoff) == "function") {
     print("custom payoff")
   } else if(payoff == "call") {
     payoff <- function(x) {
       return(pmax(0, x-exercise_price))
-      }
-    } else if(payoff == "put") {
+    }
+  } else if(payoff == "put") {
     payoff <- function(x) {
       return(pmax(0, exercise_price-x))
-      }
     }
+  }
+
+  ## the seed is set
+
+  if(!is.na(seed)) {
+    set.seed(seed)
+  }
+
+  ## the increments of the Brownian motion ###
+
+  if(antithetic == FALSE) {
+    W <-
+      c(numeric(paths), rnorm(n = paths*steps, sd = sqrt(dt))) %>%
+      matrix(nrow = paths, ncol = steps+1) %>%
+      rowCumsums()
+  } else {
+    W <- rnorm(n = paths/2*steps, sd = sqrt(dt)) %>%
+      matrix(nrow = paths/2) %>%
+      rowCumsums()
+    W <- cbind(numeric(paths), rbind(W, -W))
+  }
+
+  W_T <- W[, steps + 1]
+
+  X <- matrix(nrow = paths, ncol = steps+1)
+
+  for(i in 1:(steps + 1)) {
+      X[, i] <- initial_price *
+        exp((r-(volatility^2)/2) * (i-1)*dt + (volatility*W[, i]))
+      }
+
+  if(model == "jump_diffusion") {
+
+    Jumps <- c(numeric(paths), rpois(n = steps * paths, lambda = lambda*dt))
+
+    for(i in which(Jumps != 0)) {
+      Jumps[i] <- alpha * sum(jump_distribution(Jumps[i]))
+    }
+
+    Jumps <-
+      Jumps %>%
+      matrix(nrow = paths) %>%
+      rowCumsums()
+
+    X <- X * exp(Jumps)
+  }# model == jump
+
+  X_T <- X[, steps+1]
 
   ### the calculation of I_{(n)}, the integral \int_0^T t^n X_t dt ###
 
-  if(model == "Black Scholes") {
+  I_0 <- (X[, 1]/2 + .rowSums(X[, 2:steps], paths, (steps-1)) + X[, steps+1]/2) * dt
 
-    X <- matrix(nrow = paths, ncol = steps+1)
-
-    for(i in 1:(steps + 1)) {
-      X[, i] <- initial_price *
-        exp((r-(volatility^2)/2) * (i-1)*dt + (volatility*W[, i]))
+  if("vega" %in% greek) {
+    XW <- (X[, 1]*W[, 1]/2 + .rowSums(X[, 2:steps]*W[, 2:steps], paths, (steps-1)) +
+           X[, steps+1]*W[, steps + 1]/2) * dt
+    tXW <- X[, steps+1]*W[, steps+1]/2 * (steps*dt) * dt
+    for(i in 1:(steps-1)) {
+      tXW <- tXW + X[, i+1]*W[, i+1] * (i*dt) * dt
     }
-  } else {
-    print("Unknown model")
-    return()
   }
 
-  X_T <- X[, steps + 1]
+  if(length(intersect(greek, c("delta", "theta", "vega", "gamma")))) {
+    I_1 <- X[, steps+1]/2 * (steps*dt) * dt
+    I_2 <- X[, steps+1]/2 * (steps*dt)^2 * dt
+    for(i in 1:(steps-1)) {
+      I_1 <- I_1 + X[, i+1] * (i*dt)   * dt
+      I_2 <- I_2 + X[, i+1] * (i*dt)^2 * dt
+    }
+  }
 
-  I_0 <- dt * rowSums(X[, -1])
-
-  I_1 <- dt * rowSums(seq(from = dt, to = time_to_maturity, by = dt) * X[, -1])
-
-  I_2 <- dt * rowSums(seq(from = dt, to = time_to_maturity, by = dt)^2 * X[, -1])
-
-  I_3 <- dt * rowSums(seq(from = dt, to = time_to_maturity, by = dt)^3 * X[, -1])
-
-  XW <- dt * rowSums(X[, -1] * W[, -1])
-
-  tXW <- dt * rowSums(seq(from = dt, to = time_to_maturity, by = dt) * X[, -1] * W[, -1])
+  if("gamma" %in% greek) {
+    I_3 <- X[, steps+1]/2 * (steps*dt)^3 * dt
+    for(i in 1:(steps-1)) {
+      I_3 <- I_3 + X[, i+1] * (i*dt)^3 * dt
+    }
+  }
 
   E <- function(weight) {
     return(exp(-r*time_to_maturity) * mean(payoff(I_0/time_to_maturity) * weight))
@@ -119,8 +159,8 @@ Malliavin_Asian_Greeks <- function(initial_price = 100,
 
   if("delta" %in% greek) {
     result["delta"] <-
-      1/(volatility * initial_price) *
-      (-volatility + I_0/I_1*W_T + volatility*I_0*I_2/(I_1^2)) %>%
+      (1/(volatility * initial_price) *
+      (-volatility + I_0/I_1*W_T + volatility*I_0*I_2/(I_1^2))) %>%
       E()
   }
 
@@ -136,6 +176,26 @@ Malliavin_Asian_Greeks <- function(initial_price = 100,
       1/T +
       (1/(volatility*time_to_maturity)*I_0*W_T - 1/volatility*X_T*W_T - T*X_T)/I_1 +
       (1/T*I_0*I_2 + I_2*W_T)/(I_1^2)) %>%
+      E()
+  }
+
+  if("vega" %in% greek) {
+    result["vega"] <-
+    ((1 / volatility) *
+      ( -(1 + volatility * W_T) +
+          (W_T * XW - volatility * tXW) / I_1 +
+          (volatility * XW * I_2) / I_1^2)) %>%
+      E()
+  }
+
+  if("gamma" %in% greek) {
+    result["gamma"] <-
+      ((1/(volatility^2*initial_price^2)) *
+         (2*volatility^2
+          - 4*volatility*W_T*I_0/I_1
+          + ((W_T^2 - T)*I_0 - 4*volatility^2*I_2)*I_0/I_1^2
+          + volatility * (3*W_T*I_2 - volatility*I_3)*I_0^2/I_1^3
+          + 3*volatility^2*I_0^2*I_2^2/I_1^4)) %>%
       E()
   }
 
